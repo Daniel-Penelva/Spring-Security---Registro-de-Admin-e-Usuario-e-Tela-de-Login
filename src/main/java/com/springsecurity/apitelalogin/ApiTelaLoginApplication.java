0cc38c029a6ce3845a98b22091e747e5a20aa76f
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-10 02:15:09 -0300

    Criando Projeto para Spring Boot e Spring Security

---
+++ src/main/java/com/springsecurity/apitelalogin/ApiTelaLoginApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.apitelalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTelaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTelaLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/apitelalogin/ApiTelaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.apitelalogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTelaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
