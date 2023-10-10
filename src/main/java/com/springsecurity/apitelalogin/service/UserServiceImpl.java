3bd270393c3dd1fc809a19b579e409ca3312f20c
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-10 19:40:30 -0300

    Criando o controller (UserController)

--- src/main/java/com/springsecurity/apitelalogin/service/UserServiceImpl.java
+++ src/main/java/com/springsecurity/apitelalogin/service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.springsecurity.apitelalogin.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.springsecurity.apitelalogin.dto.UserDto;
 import com.springsecurity.apitelalogin.model.User;
 import com.springsecurity.apitelalogin.repositories.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
